f582b115742aee5cbdc12dca35e1534f5dd38693
Junyi88 <the.junyi@gmail.com>
2018-06-22 16:12:48 +0100

    ddd

--- include/utils/JUtils_GetVarType.h
+++ include/utils/JUtils_GetVarType.h
@@ -16,13 +16,20 @@ public:
   // Operators
 	JUtils_GetVarType & operator= (const JUtils_GetVarType & A); //Write to operator
 
-	void FillValue(const unsigned int &n, const unsigned int &v,
-	const unsigned int &i, const unsigned int &j, const unsigned int &t)；
+	void FillValue(const unsigned int &n, 
+		const unsigned int &v,
+		const unsigned int &i, 
+		const unsigned int &j, 
+		const unsigned int &t)；
 
-	unsigned int GetNFromV(const unsigned int &v)；
-	unsigned int GetIFromN(const unsigned int &n){return *(_IPtr+n);}；
-	unsigned int GetJFromN(const unsigned int &n){return *(_JPtr+n);}；
-	unsigned int GetTFromN(const unsigned int &n){return *(_TPtr+n);}；
+	unsigned int GetNFromV(const 
+		unsigned int &v)；
+	unsigned int GetIFromN(const 
+		unsigned int &n){return *(_IPtr+n);}；
+	unsigned int GetJFromN(const 
+		unsigned int &n){return *(_JPtr+n);}；
+	unsigned int GetTFromN(const 
+		unsigned int &n){return *(_TPtr+n);}；
 
 
 private:
